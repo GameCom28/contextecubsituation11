2723f10bcbc931dc9b824d1bf19623417c5640b2
GameCom28 <romain.martin28260@gmail.com>
2023-04-11 09:22:16 +0200

    init

---
+++ exerciceAUDIT1services.ps1
@@ -0,0 +1,21 @@
+﻿# Obtenir la date de lancement du script
+$date = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
+# Obtenir l'état des services DHCP, DNS et AD
+$dhcpService = Get-Service -Name dhcp | Select-Object Status
+$dnsService = Get-Service -Name dns | Select-Object Status
+$adService = Get-Service -Name adws | Select-Object Status
+
+
+
+# Créer le contenu du fichier texte
+$content = "Exécuter le : $date`n"
+$content += "État du service DHCP : $($dhcpService.Status)`n"
+$content += "État du service DNS : $($dnsService.Status)`n"
+$content += "État du service AD : $($adService.Status)"
+
+# Créer le fichier texte
+$filePath = "$env:HOMEDRIVE$env:HOMEPATH\Documents\$date.txt"
+Set-Content -Path $filePath -Value $content
+
+Write-Host "Le fichier a été créé : $filePath"
